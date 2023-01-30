cec07d8e0dbfb9ce05f341cd78503efe3e318fc1
Mohieb <dev.mohieb@gmail.com>
2023-01-30 22:21:12 +0300

    add projects info

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const data = () => {
     return [
         {
+            projetLink: 'https://mohieb-instagram.netlify.app/',
+            projectSrc: 'https://github.com/dev-mohieb/instagram-clone',
+            projectName: 'Instagram Clone',
+            projectImg: 'images/oldagram.png'
+        },
+        {
+            projetLink: 'https://mohieb-splitter.netlify.app/',
+            projectSrc: 'https://github.com/dev-mohieb/tip-splitter',
+            projectName: 'Tip Splitter',
+            projectImg: 'images/tip-splitter.png'
+        },
+        {
             projetLink: 'https://mohieb-rps.netlify.app/',
             projectSrc: 'https://github.com/dev-mohieb/Rock-Paper-Scissors',
             projectName: 'Rock Paper Scissors',
