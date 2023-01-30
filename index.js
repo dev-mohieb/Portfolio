69782d16cd3c1852f00c516423136518111dead9
Mohieb <dev.mohieb@gmail.com>
2023-01-30 21:09:58 +0300

    added base

---
+++ index.js
@@ -0,0 +1,10 @@
+const data = () => {
+    return [
+        {
+            projetLink: 'https://mohieb-rps.netlify.app/',
+            projectSrc: 'https://github.com/dev-mohieb/Rock-Paper-Scissors',
+            projectName: 'Rock Paper Scissors',
+            projectImg: 'images/rock-paper-scissors.png'
+        }
+    ]
+}
