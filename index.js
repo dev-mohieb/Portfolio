1074b7b96ab7216972695dbc6030f7accce564cf
Mohieb <dev.mohieb@gmail.com>
2023-01-31 22:09:19 +0300

    fixed typo that caused undefined links

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const projectsContainer = document.querySelector('.container')
 
      const projects = [
         {
-            projetLink: 'https://mohieb-instagram.netlify.app/',
+            projectLink: 'https://mohieb-instagram.netlify.app/',
             projectSrc: 'https://github.com/dev-mohieb/instagram-clone',
             projectName: 'Instagram Clone',
             projectImg: 'images/oldagram.png'
         },
         {
-            projetLink: 'https://mohieb-splitter.netlify.app/',
+            projectLink: 'https://mohieb-splitter.netlify.app/',
             projectSrc: 'https://github.com/dev-mohieb/tip-splitter',
             projectName: 'Tip Splitter',
             projectImg: 'images/tip-splitter.png'
         },
         {
-            projetLink: 'https://mohieb-rps.netlify.app/',
+            projectLink: 'https://mohieb-rps.netlify.app/',
             projectSrc: 'https://github.com/dev-mohieb/Rock-Paper-Scissors',
             projectName: 'Rock Paper Scissors',
             projectImg: 'images/rock-paper-scissors.png'
@@ -43,4 +43,4 @@ function renderProjects(data) {
     }
 }
 
-renderProjects(projects);
+renderProjects(projects);   
