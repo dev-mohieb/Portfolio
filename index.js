456b1a161eab95662606f50b91c84a9ac763a918
Mohieb <dev.mohieb@gmail.com>
2023-02-01 17:44:01 +0300

    still needs a lot of work

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
-const projectsContainer = document.querySelector('.container')
+const projectsContainer = document.querySelector(".container");
 
-     const projects = [
-        {
-            projectLink: 'https://mohieb-instagram.netlify.app/',
-            projectSrc: 'https://github.com/dev-mohieb/instagram-clone',
-            projectName: 'Instagram Clone',
-            projectImg: 'images/oldagram.png'
-        },
-        {
-            projectLink: 'https://mohieb-splitter.netlify.app/',
-            projectSrc: 'https://github.com/dev-mohieb/tip-splitter',
-            projectName: 'Tip Splitter',
-            projectImg: 'images/tip-splitter.png'
-        },
-        {
-            projectLink: 'https://mohieb-rps.netlify.app/',
-            projectSrc: 'https://github.com/dev-mohieb/Rock-Paper-Scissors',
-            projectName: 'Rock Paper Scissors',
-            projectImg: 'images/rock-paper-scissors.png'
-        }
-    ]
-
+const projects = [
+  {
+    projectLink: "https://mohieb-instagram.netlify.app/",
+    projectSrc: "https://github.com/dev-mohieb/instagram-clone",
+    projectName: "Instagram Clone",
+    projectImg: "images/oldagram.png",
+  },
+  {
+    projectLink: "https://mohieb-splitter.netlify.app/",
+    projectSrc: "https://github.com/dev-mohieb/tip-splitter",
+    projectName: "Tip Splitter",
+    projectImg: "images/tip-splitter.png",
+  },
+  {
+    projectLink: "https://mohieb-rps.netlify.app/",
+    projectSrc: "https://github.com/dev-mohieb/Rock-Paper-Scissors",
+    projectName: "Rock Paper Scissors",
+    projectImg: "images/rock-paper-scissors.png",
+  },
+];
 
 function renderProjects(data) {
-    for (let i = 0; i < data.length; i++) {
-        projectsContainer.innerHTML += `
-        <article class="project">
-          <figure>
-            <a target="_blank" href="${data[i].projectLink}"
-              ><img src="${data[i].projectImg}" alt="${data[i].projectName}"
-            /></a>
-            <figcaption class="shadow caption">
-              ${data[i].projectName}
-              <span class="git-live">
-                <a href="${data[i].projectSrc}" target="_blank" rel="noopener">Git</a>
-                <a href="${data[i].projectLink}" target="_blank" rel="noopener">Live</a>
-              </span>
-              
-            </figcaption>
-          </figure>
-        </article>`
-    }
+  for (let i = 0; i < data.length; i++) {
+    projectsContainer.innerHTML += `
+      <article class="project">
+      <img class='image' src="${data[i].projectImg}" alt="${data[i].projectName}">
+        <p class="caption">
+          ${data[i].projectName}
+        </p>
+        <span class="git-live">
+          <a href="${data[i].projectSrc}" target="_blank" rel="noopener">
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a href="${data[i].projectLink}" target="_blank" rel="noopener">
+            <i class="fa-solid fa-circle-chevron-right"></i>
+          </a>
+        </span>
+      </article>`;
+  }
 }
 
-renderProjects(projects);   
+renderProjects(projects);
